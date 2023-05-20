4f3915ab7c1cb87b06377d450c919badded46177
Ademario <ademarioteles@hotmail.com>
2023-05-20 01:51:37 -0300

    Classe Position

---
+++ xadrez-nelio-alves/chess-system/src/application/Programa.java
@@ -0,0 +1,11 @@
+package application;
+
+import boardgame.Position;
+
+public class Programa {
+
+    public static void main(String[] args){
+    Position ps = new Position(3,5);
+    System.out.println(ps.toString());
+    }
+}
---
+++ xadrez-nelio-alves/chess-system/src/boardgame/Position.java
@@ -0,0 +1,37 @@
+package boardgame;
+
+public class Position {
+    
+    private int row;
+    private int coloumn;
+
+    public Position(int row, int coloumn) {
+        this.row = row;
+        this.coloumn = coloumn;
+    }
+    
+    public int getRow() {
+        return row;
+    }
+    public int getColoumn() {
+        return coloumn;
+    }
+    
+    public void setRow(int row) {
+        this.row = row;
+    }
+    public void setColoumn(int coloumn) {
+        this.coloumn = coloumn;
+    }
+
+    @Override
+    public String toString() {
+        return   row + ", " + coloumn;
+    }
+
+   
+
+    
+
+    
+}
